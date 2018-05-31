e7a9d79afed9fcd606010b9dc39f1cfcb1165569
Xi.Chen <Xi.Chen@moodys.com>
2018-05-31 10:27:17 +0800

    add collapse component and use mock data to render it

--- src/List.js
+++ src/List.js
@@ -1,17 +1,50 @@
 import React, { Component } from 'react'
-import { Input, Button } from 'antd'
+import { Collapse } from 'antd'
 import './Add.css'
-
+const mock = [
+    {
+        name: '1',
+        text: '1231313',
+        date: '2018/5/21'
+    },
+    {
+        name: '1',
+        text: '1231313',
+        date: '2018/5/21'
+    },
+    {
+        name: '1',
+        text: '1231313',
+        date: '2018/5/21'
+    },
+    {
+        name: '1',
+        text: '1231313',
+        date: '2018/5/21'
+    }
+]
 class List extends Component {
   constructor (props) {
     super(props)
     this.state = {
     }
   }
+  renderList (data) {
+    let res = []
+    data.map((item, index) => {
+        const elem = <Panel header = {item.name} key = {index}>
+                        <p>{item.text}</p>
+                     </Panel>
+        res .push(elem)
+    })
+    return res
+  }
   render () {
     return (
       <div className='list'>
-        
+        <Collapse>
+            {this.renderList(mock)}
+        </Collapse>
       </div>
     )
   }
